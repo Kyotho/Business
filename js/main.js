f99129068afd270315a3cb86e68a2320f8cd2a97
Kyotho <jakub.jajkowicz@gmail.com>
2019-03-20 16:34:48 +0100

    layout

--- js/main.js
+++ js/main.js
@@ -23,33 +23,8 @@ function navbarCollapse() {
   }
 }
 
-// Add hamburger slidedown
 function menuSlidedown() {
   $('.hamburger').click(function() {
     $('.navbar').slideToggle('slow');
   });
 }
-
-// Add smooth scrolling
-$('#main-nav a').on('click', function(e) {
-  // Check for a hash value
-  if (this.hash !== '') {
-    // Prevent default behavior
-    e.preventDefault();
-
-    // Store hash
-    const hash = this.hash;
-
-    // Animate smooth scroll
-    $('html, body').animate(
-      {
-        scrollTop: $(hash).offset().top
-      },
-      900,
-      function() {
-        // Add hash to URL after scroll
-        window.location.hash = hash;
-      }
-    );
-  }
-});
